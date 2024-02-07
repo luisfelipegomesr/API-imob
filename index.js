91545021e6d8da0129023599107a43ccd0547668
luisfelipegomesr <luisfelipegomesr@gmail.com>
2024-02-06 21:10:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.connect((err) => {
 
 app.get("/", (req, res) => {
     console.log("Response ok.");
-    res.send("Rotas: \n " + "/clientes -> para listar os usuários. \n" + "/clientes/id -> para listar um cliente pelo código do id \n");
+    res.send("Rotas: \n " + "/clientes -> para listar os usuários. \n" + "/clientes/id -> para listar um cliente pelo código do id asdhgasidgbasidas\n");
 });
 
 
